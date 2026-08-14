c6c2941085c46a3b115a13d026bae560b17b8e1b
Kevin Brewster <kbrewster81@gmail.com>
2026-08-14 14:12:59 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-import Highlight from "./components//ui/Highlight";
+import Highlight from "./components/ui/Highlight";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Landing />
-      <Highlight />
+      <Highlights />
     </div>
   );
 }
